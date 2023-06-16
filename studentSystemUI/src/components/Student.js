888fc9106c3e00425f548167b0c18c6e9778b863
Sumeet Patil <spatil0296@gmail.com>
2023-06-16 18:22:00 -0400

    Code Cleanup

--- studentSystemUI/src/components/Student.js
+++ studentSystemUI/src/components/Student.js
@@ -1,26 +1,14 @@
 import React, { useEffect, useState } from 'react';
-// import { makeStyles } from '@mui/styles';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { Paper } from '@mui/material';
 import { Container } from '@mui/material';
-
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     '& > *': {
-//       margin: theme.spacing(1),
-
-//     },
-//   },
-// }));
 
 export default function BasicTextFields() {
   const paperStyle = { padding: '50px 20px', width: 600, margin: "20px auto" }
   const [name, setName] = useState('')
   const [address, setAddress] = useState('')
   const [students, setStudents] = useState([])
-  //const classes = useStyles();
 
   const handleClick = (e) => {
     e.preventDefault()
@@ -49,8 +37,6 @@ export default function BasicTextFields() {
     <Container>
       <Paper elevation={3} style={paperStyle}>
         <h1 style={{ color: "blue" }}><u>Add Student</u></h1>
-
-        {/* <form className={classes.root} noValidate autoComplete="off"> */}
       <form>
           <TextField id="outlined-basic" label="Student Name" variant="outlined" fullWidth
             value={name}
@@ -79,12 +65,7 @@ export default function BasicTextFields() {
           </Paper>
         ))
         }
-
-
       </Paper>
-
-
-
     </Container>
   );
 }
